a6a7e0d418b6a0addd4b27d0484b8793033b6933
nicolae <stroncea.nicolae@gmail.com>
2020-06-11 22:07:50 -0400

    working calendar version

--- synchronize.py
+++ synchronize.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-from datetime import datetime
+import os
+from datetime import datetime, timedelta, timezone
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -16,19 +17,15 @@ calendarId = 'primary'
 def get_token():
     try:
         with open('token.txt','r') as f:
-            token = f.readline()
+            date, token  = f.readline().split(' ')
     except FileNotFoundError:
         token = None
-    return token
+        date = None
+    return token, date
 
 def store_token(token):
     with open('token.txt','w') as f:
-        f.write(token)
-
-def calendar_list():
-    service = build('calendar', 'v3', credentials=get_credentials())
-    calendar_list = service.calendarList().list().execute()
-    return calendar_list
+        f.write(str(datetime.now().date()) + " " + token)
 
 def get_credentials():
     creds = None
@@ -53,104 +50,84 @@ def get_credentials():
 
 def capture_events():
     events_list = []
-    syncToken = get_token()
+    syncToken, date_stored = get_token()
+    time_max = datetime.now(timezone.utc) + timedelta(days=2)
+    time_max = datetime.strftime(time_max, '%Y-%m-%dT%H:%M:%S%z')
     service = build('calendar', 'v3', credentials=get_credentials())
     # Call the Calendar API
-    events_result = service.events().list(calendarId=calendarId,
-                                        syncToken=syncToken).execute()
-    events = events_result.get('items', [])
-    events_list.append(events)
+    # if token date does not exist(meaning no syncing previously) or not the same as today, automatically do a new backup.
+    # This is necessary because we're passing the timeMax parameter when doing a full sync so that we only store events in the 
+    # database for a max of 2 days from now. Therefore do a full sync every new day to get the events after 2 days from now.
+    if (date_stored is None or date_stored != str(datetime.now().date())):
+        print("token date is None or not today")
+        events_result = service.events().list(calendarId=calendarId, singleEvents=True, timeMax=str(time_max)).execute()
+        events = events_result.get('items', [])
+    else: # dates are the same, so first try an incremental sync(using synctoken) to check if there are any new changes
+        events_result = service.events().list(calendarId=calendarId, syncToken=syncToken, singleEvents=True).execute()
+        events = events_result.get('items', [])
+        if(len(events) == 0):
+            print("no new events")
+            return events_list
+        # there is at least one new event, so have to do a full sync
+        else:
+            print("found new events")
+            events_result = service.events().list(calendarId=calendarId, singleEvents=True, timeMax=str(time_max)).execute()
+            events = events_result.get('items', [])
+    events_list.extend(events)
     # We have captured all of the events since the last token, only when the response
     # returns the nextSyncToken
     nextSyncToken = events_result.get('nextSyncToken',None)
     while nextSyncToken is None:
         nextPageToken = events_result.get('nextPageToken',None)
-        events_result = service.events().list(calendarId=calendarId,
-            syncToken=syncToken, pageToken=nextPageToken).execute()
+        events_result = service.events().list(calendarId=calendarId, singleEvents=True, pageToken=nextPageToken, timeMax=str(time_max)).execute()
         events = events_result.get('items', [])
-        events_list.append(events)
+        events_list.extend(events)
         nextSyncToken = events_result.get('nextSyncToken',None)
     store_token(nextSyncToken)
     return events_list
 
 def interpret_events(list_of_events):
     events_to_insert = []
-    cancelled_event_ids = []
-    for event_list in list_of_events:
-        for event in event_list:
-            #TODO figure out timezone
-            print(event)
-            if event['status'] in ['confirmed', 'tentative']:
-                event_id = event['id']
-                status = 1
-                label = event['summary']
-                datastr = {'event_id':event_id, 'label':label, 'status':status}
-                if ('recurrence' in event.keys()): datastr['recurrence'] = str(event['recurrence'])
-                if ('recurringEventId' in event.keys()): datastr['recurringEventId'] = event['recurringEventId'] 
-                if 'originalStartTime' in event.keys():
-                    originalStartTime = event['originalStartTime'].get('dateTime', event['originalStartTime'].get('date'))
-                    datastr['originalStartTime'] = originalStartTime
-                    originalTimeZone = event['originalStartTime'].get('timeZone')
-                if event['start'].get('dateTime') != None:
-                    start = event['start'].get('dateTime')
-                    start_time = datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z')
-                else:
-                    start = event['start'].get('date')
-                    start_time = datetime.strptime(start, '%Y-%m-%d')
-                start_timezone = event['start'].get('timeZone', None)
-                if event['end'].get('dateTime') != None:
-                    end = event['end'].get('dateTime')
-                    end_time = datetime.strptime(end, '%Y-%m-%dT%H:%M:%S%z')
-                else:
-                    end = event['end'].get('date')
-                    end_time = datetime.strptime(end, '%Y-%m-%d')
-                end_timezone = event['end'].get('timeZone', None)
+    for event in list_of_events:
+        label = event['summary']
+        datastr = {'label':label}
+        if event['start'].get('dateTime') != None:
+            start = event['start'].get('dateTime')
+            start_time = datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z')
+        else:
+            start = event['start'].get('date')
+            start_time = datetime.strptime(start, '%Y-%m-%d')
+        if event['end'].get('dateTime') != None:
+            end = event['end'].get('dateTime')
+            end_time = datetime.strptime(end, '%Y-%m-%dT%H:%M:%S%z')
+        else:
+            end = event['end'].get('date')
+            end_time = datetime.strptime(end, '%Y-%m-%d')
 
-                duration = end_time - start_time
-                event = Event(timestamp=start_time, duration=duration, data=datastr)
-                events_to_insert.append(event)
-                
-            else: # event is cancelled
-                event_id = event['id']
-                status = 0
-                # check if the event is part of an uncancelled recurring event.
-                # If it is, that means that it needs to be stored as this instance
-                # should no longer be shown to the user
-                # otherwise this event needs to be deleted from the database.
-                
-                # event should be deleted from database
-                if 'recurringEventId' not in event.keys() and 'originalStartTime' not in event.keys():
-                    cancelled_event_ids.append(event_id)
-                else: # it's part of a still existing recurring event
-                    recurringEventId = event['recurringEventId'] if 'recurringEventId' in event.keys() else ''
-                    if event['originalStartTime'].get('dateTime') != None:
-                        originalStartTime = event['originalStartTime'].get('dateTime')
-                        print("originalStartTime", originalStartTime)
-                    else:
-                        originalStartTime = datetime.strptime(originalStartTime, '%Y-%m-%dT%H:%M:%S%z')
-                        originalStartTime = event['originalStartTime'].get('date')
-                        originalStartTime = datetime.strptime(originalStartTime, '%Y-%m-%d')
-                        print("originalStartTime", originalStartTime)
+        duration = end_time - start_time
+        event = Event(timestamp=start_time.isoformat(), duration=duration, data=datastr)
+        events_to_insert.append(event)
 
-                    originalTimeZone = event['originalStartTime'].get('timeZone')
-                    event = Event(timestamp = originalStartTime, duration = 0,
-                    data = {'event_id': event_id, 'status':status,'recurringEventId':recurringEventId,'originalStartTime':str(originalStartTime)})      
-                    events_to_insert.append(event)
+    return events_to_insert
 
-    return events_to_insert, cancelled_event_ids
 
-if __name__ == "__main__":
-    events_to_insert, cancelled_event_ids = interpret_events(capture_events())
-    print(events_to_insert)
+captured_events = capture_events()
 
-    aw = ActivityWatchClient(testing=True)
 
-    bucket_name = 'aw-calendar-import'
+events_to_insert = interpret_events(captured_events)
 
-    if bucket_name not in aw.get_buckets():
+if(events_to_insert):
+    try:
+        aw = ActivityWatchClient(testing=True)
+        bucket_name = 'aw-calendar-import'
+        try:
+            aw.delete_bucket(bucket_name)
+        except:
+            print("Bucket doesn't exist") 
         aw.create_bucket(bucket_name, 'calendar')
-    
-    aw.insert_events(bucket_name, events_to_insert)
-    for id_to_del in cancelled_event_ids:
-        # aw.delete_calendar_event(bucket_name, id_to_del)
-        pass
+        aw.insert_events(bucket_name, events_to_insert)
+    except:
+        print("something went wrong")
+        if os.path.exists("token.txt"):
+            os.remove("token.txt")
+        exit
